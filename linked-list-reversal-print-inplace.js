87f8cf486da0788f212df050eed095321a638a1c
Hariharan <hariharan.s@samsung.com>
2021-02-11 08:53:32 +0530

    Medium problems

---
+++ linked-list-reversal-print-inplace.js
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/print-immutable-linked-list-in-reverse/submissions/
+
+/**
+ * // This is the ImmutableListNode's API interface.
+ * // You should not implement it, or speculate about its implementation.
+ * function ImmutableListNode() {
+ *    @ return {void}
+ *    this.printValue = function() { // print the value of this node.
+ *       ...
+ *    }; 
+ *
+ *    @return {ImmutableListNode}
+ *    this.getNext = function() { // return the next node.
+ *       ...
+ *    };
+ * };
+ */
+
+/**
+ * @param {ImmutableListNode} head
+ * @return {void}
+ */
+
+function listReverse(head) {
+  if (head.getNext()) {
+      listReverse(head.getNext());
+  }
+  head.printValue();
+}
+
+var printLinkedListInReverse = function(head) {
+  listReverse(head);
+};
