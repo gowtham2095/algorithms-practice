6ebedba76f6bf5ea63e887e5b53a706249889edf
Hariharan <gowtham2095@gmail.com>
2021-03-05 01:05:16 +0530

    Easy probs

---
+++ algorithms/canPerumtePalindrome.js
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/palindrome-permutation/submissions/
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var canPermutePalindrome = function(s) {
+  let mapStr = {};
+  let str = s.split('');
+  
+  str.forEach((ch) => {
+    if (!mapStr[ch]) {
+        mapStr[ch] = 1;
+    } else {
+        delete mapStr[ch];
+    } 
+  })
+  
+  let keys = Object.keys(mapStr);
+  return keys.length > 1 ? false : true;
+};
